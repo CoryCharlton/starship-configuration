e81213f8197673f37a0c5928266d8abf3ce99829
Cory Charlton <CoryCharlton@users.noreply.github.com>
2024-09-22 11:44:36 -0700

    Removing shell configs

--- shells/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,2 +0,0 @@
-$ENV:STARSHIP_CONFIG = "C:\src\starship-configuration\starship.toml"
-Invoke-Expression (&starship init powershell)
--- shells/clink/starship.lua
+++
@@ -1,2 +0,0 @@
-os.setenv('STARSHIP_CONFIG', 'C:\\src\\starship-configuration\\starship.toml')
-load(io.popen('starship init cmd'):read("*a"))()
